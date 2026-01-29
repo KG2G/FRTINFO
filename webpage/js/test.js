20ba4c48504718dd4592df684a1b2dd5f04b942a
KG2G <AlnzicOt@protonmail.com>
2026-01-29 20:45:47 +0000

    Delete webpage/js directory

--- webpage/js/test.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.fancy-btn').addEventListener('click', function() {
-    this.textContent = 'Clicked! 🎉';
-    setTimeout(() => {
-        this.textContent = 'Click Me!';
-    }, 1000);
-});
